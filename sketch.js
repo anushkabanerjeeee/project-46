8486b3fee4efb4623df2bd2998f706e61e36c24f
anushkabanerjeeee <115883342+anushkabanerjeeee@users.noreply.github.com>
2023-04-14 12:48:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,150 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting,heart1,hear2,heart3;
+var ghost,ghostGroup,ghostImg,bullet,bulletImg,bulletGroup,bullets=90,heartImg1,heartImg2,heartImg3
+var life = 3
+var score = 0
+
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/background1.jpg")
+  ghostImg = loadImage("assets/ghost.png")
+  bulletImg = loadImage("assets/bullet.png")
+
+  heartImg1 = loadImage("assets/heart_1.png")
+  heartImg2 = loadImage("assets/heart_2.png")
+  heartImg3 = loadImage("assets/heart_3.png")
+
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40)
+bg.addImage(bgImg)
+bg.scale = 0.4
+
+ghostGroup = new Group()
+bulletGroup = new Group()
+
+heart1 =  createSprite(displayWidth-150,40,20,20)
+heart1.addImage("heart1",heartImg1)
+heart1.visible = false
+heart1.scale = 0.5
+
+heart2 =  createSprite(displayWidth-150,40,20,20)
+heart2.addImage("heart2",heartImg2)
+heart2.visible = false
+heart2.scale = 0.5
+
+heart3 =  createSprite(displayWidth-200,40,20,20)
+heart3.addImage("heart3",heartImg3)
+heart3.visible = true
+heart3.scale = 0.5
+
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+}
+
+function draw() {
+  background(0); 
+if(life == 3){
+  heart1.visible = false
+  heart2.visible = false
+  heart3.visible = true
+}
+if(life == 2){
+  heart1.visible = false
+  heart2.visible = true
+  heart3.visible = false
+}
+if(life == 1){
+  heart1.visible = true
+  heart2.visible = false
+  heart3.visible = false
+}
+spawnGhost()
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ bullet = createSprite(displayWidth-1150,player.y-30,20,10)
+ bullet.addImage(bulletImg)
+ bullet.scale = 0.04
+ bullet.velocityX = 20
+ bulletGroup.add(bullet)
+ bullet.depth=player.depth
+ player.depth=player.depth+1
+ if(bullets>0){
+  bullets = bullets - 1
+ }
+
+ 
+
+  player.addImage(shooter_shooting)
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+if(ghostGroup.isTouching(bulletGroup)){
+  for(var i=0;i<ghostGroup.length;i++){
+    if(ghostGroup[i].isTouching(bulletGroup)){
+      ghostGroup[i].destroy()
+      bulletGroup.destroyEach()
+      score = score + 2
+    }
+  }
+}
+if(ghostGroup.isTouching(player)){
+  for(var i=0;i<ghostGroup.length;i++){
+    if(ghostGroup[i].isTouching(player)){
+      ghostGroup.destroyEach()
+      life=life-1
+    }
+  }
+}
+drawSprites();
+textSize(20)
+fill("white")
+text("Score:"+score,displayWidth-230,displayHeight/2-230)
+text("Life:"+life,displayWidth-230,displayHeight/2-260)
+text("Bullets:"+bullets,displayWidth-230,displayHeight/2-290)
+
+}
+function spawnGhost(){
+  if(frameCount%60===0){
+    ghost=createSprite(random(500,1100),random(100,500),40,40)
+ghost.addImage(ghostImg)
+ghost.velocityX = -3
+ghost.scale = 0.4
+ghost.lifetime = 500
+ghostGroup.add(ghost)
+  }
+
+}
